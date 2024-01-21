7ae0cb56430f6a821abebad6e5ba0db20355fd81
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-01-21 17:30:10 +0500

    'added'

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,9 +16,9 @@ mongoose.connect(process.env.MONGO_URI as string)
 
 // CLOUD_NARY CONFIG!
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
 })
 
 // NECESSARY MIDDLEWARES!!
