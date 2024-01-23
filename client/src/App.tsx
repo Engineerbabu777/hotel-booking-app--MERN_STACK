365d64662a7a4fc3c089a883121642e7c048cb46
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-01-23 19:43:23 +0500

    new:my-hotels-page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
-// import MyHotels from "./pages/MyHotels";
+import MyHotels from "./pages/MyHotels";
 // import EditHotel from "./pages/EditHotel";
 // import Search from "./pages/Search";
 // import Detail from "./pages/Detail";
@@ -90,14 +90,14 @@ const App = () => {
                 </Layout>
               }
             /> */}
-            {/* <Route
+            <Route
               path="/my-hotels"
               element={
                 <Layout>
                   <MyHotels />
                 </Layout>
               }
-            /> */}
+            />
             {/* <Route
               path="/my-bookings"
               element={
